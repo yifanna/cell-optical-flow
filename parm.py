e603f68ff2dc4c37e2a48d5b013a45250afd7d0a
yifanna <146762057+yifanna@users.noreply.github.com>
2025-07-14 21:16:50 +0800

    Update parm.py

--- parm.py
+++ parm.py
@@ -1,15 +1,4 @@
-# import torch
- 
-# model_dict = torch.load("C:/Users/m1384/Desktop/100000_raft-sintel.pth")
-# # print(model_dict.keys())
-# params_dict=model_dict['state_dict']
- 
-# total_params = 0
-# for param_tensor in params_dict.values():
-#     # 将当前参数的元素数（即参数大小）加到总和中
-#     total_params += param_tensor.numel()
- 
-# print(f"参数量约为：{total_params/1000000:.2f}M（百万个参数）。")
+
 import torch
 
 # 加载模型参数
